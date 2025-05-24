3efbf91e61e8f624fff232ae51ef207070b5b222
AsValen <75420108+AsValen@users.noreply.github.com>
2025-05-24 21:20:33 +0800

    implement spawn env script

---
+++ MMUGameJam2025/Assets/Scripts/SpawnEnv.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnv : MonoBehaviour
+{
+    [SerializeField] private GameObject seaEnv;
+    [SerializeField] private GameObject forestEnv;
+    [SerializeField] private GameObject skyEnv;
+
+    private GameObject currentEnv;
+    private bool reachedDestination = false;
+
+    public float currentkiloDistance;
+    public float TotalkiloDistance;
+    public float goalDistance = 5000f;
+
+    private float timeToChangeEnv = 2f;
+
+
+    void Awake()
+    {
+        if (currentEnv == null)
+        {
+            randomChooseEnv();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(currentEnv == null)
+        //{
+        //    randomChooseEnv();
+        //} 
+        //else if (currentkiloDistance >= goalDistance)
+        //{
+        //    currentkiloDistance = 0f;
+        //    StartCouroutine(prepareNextEnv());
+        //}
+    }
+
+    IEnumerator prepareNextEnv()
+    {
+        // FLASH BANG OF WHITENESS, put here
+        yield return new WaitForSeconds(timeToChangeEnv); // Wait for 2 seconds before spawning the next environment
+        randomChooseEnv();
+    }
+
+    void randomChooseEnv()
+    {
+        int randomEnv = Random.Range(0, 3);
+        switch (randomEnv)
+        {
+            case 0:
+                currentEnv = Instantiate(seaEnv, transform.position, Quaternion.identity);
+                break;
+            case 1:
+                currentEnv = Instantiate(forestEnv, transform.position, Quaternion.identity);
+                break;
+            case 2:
+                currentEnv = Instantiate(skyEnv, transform.position, Quaternion.identity);
+                break;
+        }
+    }
+
+}
