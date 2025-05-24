de1b9b9b1da48102ea056fcf5eb08536efa7e725
AsValen <75420108+AsValen@users.noreply.github.com>
2025-05-25 04:14:14 +0800

    km, change bg

--- MMUGameJam2025/Assets/Scripts/SpawnEnv.cs
+++ MMUGameJam2025/Assets/Scripts/SpawnEnv.cs
@@ -11,9 +11,11 @@ public class SpawnEnv : MonoBehaviour
     public GameObject currentEnv;
     private bool reachedDestination = false;
 
+    public ZDistanceCount zDistanceCount;
     public float currentkiloDistance;
     public float TotalkiloDistance;
-    public float goalDistance = 5000f;
+    public float lastDistanceCovered = 0f;
+    public float goalDistance = 15f;
 
     private float timeToChangeEnv = 2f;
 
@@ -30,19 +32,31 @@ public class SpawnEnv : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if(currentEnv == null)
-        //{
-        //    randomChooseEnv();
-        //} 
-        //else if (currentkiloDistance >= goalDistance)
-        //{
-        //    currentkiloDistance = 0f;
-        //    StartCouroutine(prepareNextEnv());
-        //}
+        if (currentEnv == null)
+        {
+            randomChooseEnv();
+            return; // Make sure the rest doesn't run until an environment is chosen
+        }
+
+        TotalkiloDistance = zDistanceCount.distanceTravelled;
+        currentkiloDistance = TotalkiloDistance - lastDistanceCovered;
+
+
+        Debug.Log("Current Distance: " + currentkiloDistance);
+        Debug.Log("goal: " + goalDistance);
+
+        if (currentkiloDistance >= goalDistance)
+        {
+            Debug.Log("1");
+            lastDistanceCovered = TotalkiloDistance;
+            currentkiloDistance = 0f;
+            StartCoroutine(prepareNextEnv());
+        }
     }
 
     IEnumerator prepareNextEnv()
     {
+        Debug.Log("2");
         // FLASH BANG OF WHITENESS, put here
         yield return new WaitForSeconds(timeToChangeEnv); // Wait for 2 seconds before spawning the next environment
         randomChooseEnv();
@@ -50,16 +64,19 @@ public class SpawnEnv : MonoBehaviour
 
     void randomChooseEnv()
     {
-        int randomEnv = Random.Range(0, 3);
+        Debug.Log("Choosing a new environment...");
+        int randomEnv = Random.Range(0, 2);
+        Destroy(currentEnv);
+
         switch (randomEnv)
         {
             case 0:
                 currentEnv = Instantiate(seaEnv, transform.position, Quaternion.identity);
                 break;
+            //case 1:
+            //    currentEnv = Instantiate(forestEnv, transform.position, Quaternion.identity);
+            //    break;
             case 1:
-                currentEnv = Instantiate(forestEnv, transform.position, Quaternion.identity);
-                break;
-            case 2:
                 currentEnv = Instantiate(skyEnv, transform.position, Quaternion.identity);
                 break;
         }
